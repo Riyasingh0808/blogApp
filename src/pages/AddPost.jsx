195c8f797cc8c74d9616c2286bd7d5db30b568f0
Riyasingh0808 <167004426+Riyasingh0808@users.noreply.github.com>
2024-07-26 20:51:13 +0530

    first commit

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import{Container, PostForm} from '../components'
+
+function Addpost(){
+    return(
+        <div classname='py-8'>
+            <container>
+                <PostForm/>
+            </container>
+        </div>
+    )
+}
+export default Addpost
